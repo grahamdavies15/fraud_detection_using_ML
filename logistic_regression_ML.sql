8d4fd103d9a64e2e12411ec883fd1697e0925269
Graham Davies <86721524+grahamdavies15@users.noreply.github.com>
2025-05-12 11:36:45 +0100

    Create logistic_regression_ML.sql

---
+++ logistic_regression_ML.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE MODEL
+  finance.model_supervised_initial
+  OPTIONS(model_type='LOGISTIC_REG', INPUT_LABEL_COLS = ["isfraud"]
+  )
+AS
+SELECT
+type, amount, oldbalanceOrig, newbalanceOrig, oldbalanceDest, newbalanceDest, isFraud
+FROM finance.fraud_data_model;
+
+SELECT
+  *
+FROM
+  ML.WEIGHTS(MODEL `finance.model_supervised_initial`,
+    STRUCT(true AS standardize));
